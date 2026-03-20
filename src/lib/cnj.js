babf12ea8d9d477d62a6362c6d2267ee4efcec2f
Nasci15k <diniz.nascimento1905@gmail.com>
2026-03-20 20:00:10 -0300

    Update cnj.js

--- src/lib/cnj.js
+++ src/lib/cnj.js
@@ -1,2 +1,2 @@
 // Re-exporta do módulo consolidado correto
-export { fetchMandadosPrisao, fetchDataJud, fetchSisbajud } from './cnj_tcu_cvm_ibge.js';
+export { fetchMandadosPrisao, fetchDataJud, fetchSisbajud, fetchTCUIrregularidades } from './cnj_tcu_cvm_ibge.js';

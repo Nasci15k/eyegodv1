c3a4d496459a376752c49fbd61e1d806bed51505
Nasci15k <diniz.nascimento1905@gmail.com>
2026-03-20 19:57:39 -0300

    Update cvm_tcu.js

--- src/lib/cvm_tcu.js
+++ src/lib/cvm_tcu.js
@@ -1,1 +1,3 @@
-export { fetchCVMInfo, fetchTCUIrregularidades } from './cnj_tcu_cvm_ibge.js';
+// Re-exporta do módulo consolidado correto
+export { fetchCVMInfo } from './cnj_tcu_cvm_ibge.js';
+export { fetchTCUIrregularidades } from './cnj_tcu_cvm_ibge.js';
